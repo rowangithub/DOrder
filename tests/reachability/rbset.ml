102a291ffccbcc9b9189f9344bf3cc0f9a9ceffb
herowanzhu <rowangithub@yahoo.com>
2015-12-21 21:49:55 -0500

    rbset work again

--- tests/reachability/rbset.ml
+++ tests/reachability/rbset.ml
@@ -1,7 +1,10 @@
+	type color = 
+		| Red 
+		| Black
+	
 	type 'a t = 
 		| Empty 
-		| Black of 'a t * 'a * 'a t 
-		| Red of 'a t * 'a * 'a t
+		| Node of color * 'a t * 'a * 'a t 
 
 	type 'a enum =
     | End
@@ -10,8 +13,8 @@
   let rec enum s e =
     match s with
       | Empty -> e
-      | Black(l, x, n) -> enum l (More(x, n, e))
-			| Red(l, x, n) -> enum l (More(x, n, e))
+      | Node (Black, l, x, n) -> enum l (More (x, n, e))
+			| Node (Red, l, x, n) -> enum l (More (x, n, e))
 
   (* Invariants: (1) a red node has no red son, and (2) any path from the
      root to a leaf has the same number of black nodes *)
@@ -21,7 +24,7 @@
      These red-black trees saves 20\% of space w.n.t Ocaml's AVL, which
      store the height into a fourth argument. *)
 
-  (*s Implementation of the set operations; [empty], [is_empty], [mem]
+  (*s Implementation of the [empty], [is_empty], [mem]
       and [singleton] are trivial. *)
 
   let empty = Empty
@@ -34,86 +37,84 @@
   let rec mem x t = 
 		match t with
     | Empty -> false
-    | Black (l, dv, n) ->
+    | Node (c, l, dv, n) ->
 			if x = dv then true
 			else if x < dv then
 				mem x l
-			else mem x n
-		| Red (l, dv, n) ->
-      if x = dv then true
-			else if x < dv then
-				mem x l
-			else mem x n
+			else 
+				mem x n
 
-  let singleton x = Black (Empty, x, Empty)
+  let singleton x = 
+		Node (Black, Empty, x, Empty)
 
   (*s Insertion *)
 
   let lbalance x2 x1 x3 = 
 		match x1, x2, x3 with
-    | Red (Red (a,x,b), y, c), z, d ->
-        Red (Black (a,x,b), y, Black (c,z,d))
-    | Red (a, x, Red (b,y,c)), z, d ->
-        Red (Black (a,x,b), y, Black (c,z,d))
+    | Node (Red, Node (Red,a,x,b), y, c), z, d ->
+        Node (Red, Node (Black,a,x,b), y, Node (Black,c,z,d))
+    | Node (Red, a, x, Node (Red,b,y,c)), z, d ->
+        Node (Red, Node (Black,a,x,b), y, Node (Black,c,z,d))
     | a,x,b ->
-        Black (a,x,b)
+        Node (Black,a,x,b)
 
   let rbalance x2' x1' x3' = 
 		match x1', x2', x3' with
-    | a, x, Red (Red (b,y,c), z, d) ->
-        Red (Black (a,x,b), y, Black (c,z,d))
-    | a, x, Red (b, y, Red (c,z,d)) ->
-        Red (Black (a,x,b), y, Black (c,z,d))
+    | a, x, Node (Red, Node (Red,b,y,c), z, d) ->
+        Node (Red, Node (Black,a,x,b), y, Node (Black,c,z,d))
+    | a, x, Node (Red, b, y, Node (Red,c,z,d)) ->
+        Node (Red, Node (Black, a,x,b), y, Node (Black, c,z,d))
     | a,x,b ->
-        Black (a,x,b)
+        Node (Black, a,x,b)
 
 	let rec ins z s = 
 		match s with 
       | Empty ->
-          Red (Empty, z, Empty)
-      | Red (a, y, b) ->
-          if z < y then Red (ins z a, y, b)
-          else if z > y then Red (a, y, ins z b)
+          Node (Red, Empty, z, Empty)
+      | Node (Red, a, y, b) ->
+          if z < y then Node (Red, ins z a, y, b)
+          else if z > y then Node (Red, a, y, ins z b)
           else s
-      | Black (a, y, b) ->
+      | Node (Black, a, y, b) ->
           if z < y then lbalance y (ins z a) b
           else if z > y then rbalance y a (ins z b)
           else s
 
   let add z s =
     match ins z s with
-      | Black (a, y, b) -> Black (a, y, b)
-      | Red (a, y, b) -> Black (a, y, b)
+      | Node (Black, a, y, b) -> 
+				Node (Black, a, y, b)
+      | Node (Red, a, y, b) -> 
+				Node (Black, a, y, b)
       (*| Empty -> assert false*)
-	let harness () = add 0 Empty
-
-  (*s Removal *)
+	
+	(*s Removal *)
 
   (* [unbalanced_left] repares invariant (2) when the black height of the
      left son exceeds (by 1) the black height of the right son *)
 
-  let unbalanced_left t = 
-		match t with
-    | Red (Black (t1, x1, t2), x2, t3) ->
-        lbalance x2 (Red (t1, x1, t2)) t3, false
-    | Black (Black (t1, x1, t2), x2, t3) ->
-        lbalance x2 (Red (t1, x1, t2)) t3, true
-    | Black (Red (t1, x1, Black (t2, x2, t3)), x3, t4) ->
-        Black (t1, x1, lbalance x3 (Red (t2, x2, t3)) t4), false
+  let unbalanced_left lt = 
+		match lt with
+    | Node (Red, Node (Black, t1, x1, t2), x2, t3) ->
+        lbalance x2 (Node (Red, t1, x1, t2)) t3, false
+    | Node (Black, Node (Black, t1, x1, t2), x2, t3) ->
+        lbalance x2 (Node (Red, t1, x1, t2)) t3, true
+    | Node (Black, Node (Red, t1, x1, Node (Black, t2, x2, t3)), x3, t4) ->
+        Node (Black, t1, x1, lbalance x3 (Node (Red, t2, x2, t3)) t4), false
     (*| _ ->
         assert false*)
 
   (* [unbalanced_right] repares invariant (2) when the black height of the
      right son exceeds (by 1) the black height of the left son *)
 
-  let unbalanced_right t =
-		match t with
-    | Red (t1, x1, Black (t2, x2, t3)) ->
-        rbalance x1 t1 (Red (t2, x2, t3)), false
-    | Black (t1, x1, Black (t2, x2, t3)) ->
-        rbalance x1 t1 (Red (t2, x2, t3)), true
-    | Black (t1, x1, Red (Black (t2, x2, t3), x3, t4)) ->
-        Black (rbalance x1 t1 (Red (t2, x2, t3)), x3, t4), false
+  let unbalanced_right rt =
+		match rt with
+    | Node (Red, t1, x1, Node (Black, t2, x2, t3)) ->
+        rbalance x1 t1 (Node (Red, t2, x2, t3)), false
+    | Node (Black, t1, x1, Node (Black, t2, x2, t3)) ->
+        rbalance x1 t1 (Node (Red, t2, x2, t3)), true
+    | Node (Black, t1, x1, Node (Red, Node (Black, t2, x2, t3), x3, t4)) ->
+        Node (Black, rbalance x1 t1 (Node (Red, t2, x2, t3)), x3, t4), false
     (*| _ ->
         assert false*)
 
@@ -121,32 +122,34 @@
      resulting set, and indicates with [b] whether the black height has
      decreased *)
 
-  let rec remove_min t = 
-		match t with
+  let rec remove_min tm = 
+		match tm with
     (*| Empty ->
 			assert false*)
     (* minimum is reached *)
-    | Black (Empty, x, Empty) ->
+    | Node (Black, Empty, x, Empty) ->
 			Empty, x, true
-    | Black (Empty, x, Red (l, y, n)) ->
-			Black (l, y, n), x, false
+    | Node (Black, Empty, x, Node (Red, l, y, n)) ->
+			Node (Black, l, y, n), x, false
     (*| Black (Empty, _, Black _) ->
 			assert false*)
-    | Red (Empty, x, n) ->
+    | Node (Red, Empty, x, n) ->
 			n, x, false
     (* minimum is recursively extracted from [l] *)
-    | Black (l, x, n) ->
+    | Node (Black, l, x, n) ->
 			let l',m,d = remove_min l in
-			let t = Black (l', x, n) in
+			let t = Node (Black, l', x, n) in
 			if d then
-	  		let t,d' = unbalanced_right t in t,m,d'
+	  		let t,d' = unbalanced_right t in 
+				t,m,d'
 			else
 	  		t, m, false
-    | Red (l, x, n) ->
+    | Node (Red, l, x, n) ->
 			let l',m,d = remove_min l in
-			let t = Red (l', x, n) in
+			let t = Node (Red, l', x, n) in
 			if d then
-	  		let t,d' = unbalanced_right t in t,m,d'
+	  		let t,d' = unbalanced_right t in 
+				t,m,d'
 			else
 	  		t, m, false
 
@@ -158,59 +161,69 @@
 		match t with
       | Empty ->
 	  		Empty, false
-      | Black (l, y, n) ->
+      | Node (Black, l, y, n) ->
 			  if x < y then
 			    let l',d = remove_aux x l in
-			    let t = Black (l', y, n) in
-			    if d then unbalanced_right t else t, false
+			    let t = Node (Black, l', y, n) in
+			    if d then unbalanced_right t 
+					else t, false
 			  else if x > y then
 			    let n',d = remove_aux x n in
-			    let t = Black (l, y, n') in
-			    if d then unbalanced_left t else t, false
+			    let t = Node (Black, l, y, n') in
+			    if d then unbalanced_left t 
+					else t, false
 			  else (* x = y *)
 			    (match n with
 			       | Empty ->
 				   			(match l with
-									| Red (l, x, n) -> Black (l, x, n), false
-    							| Black (a, b, c) -> l, true
-									| Empty -> l, true
+									| Node (Red, l, x, n) -> 
+										Node (Black, l, x, n), false
+    							| Node (Black, a, b, c) -> 
+										l, true
+									| Empty -> 
+										l, true
 								)
-			       | Red (a, b, c) ->
+			       | Node (Red, a, b, c) ->
 				   			let n',m,d = remove_min n in
-				   			let t = Black (l, m, n') in
-				   			if d then unbalanced_left t else t, false
+				   			let t = Node (Black, l, m, n') in
+				   			if d then unbalanced_left t 
+								else t, false
 								
-						 | Black (a, b, c) ->		
+						 | Node (Black, a, b, c) ->		
 								let n',m,d = remove_min n in
-				   			let t = Black (l, m, n') in
-				   			if d then unbalanced_left t else t, false
+				   			let t = Node (Black, l, m, n') in
+				   			if d then unbalanced_left t 
+								else t, false
 						)
-  	 | Red (l, y, n) ->
+  	 | Node (Red, l, y, n) ->
   			if x < y then
     			let l',d = remove_aux x l in
-    			let t = Red (l', y, n) in
-    			if d then unbalanced_right t else t, false
+    			let t = Node (Red, l', y, n) in
+    			if d then unbalanced_right t 
+					else t, false
   			else if x > y then
     			let n',d = remove_aux x n in
-    			let t = Red (l, y, n') in
-    			if d then unbalanced_left t else t, false
+    			let t = Node (Red, l, y, n') in
+    			if d then unbalanced_left t 
+					else t, false
   			else (* x = y *)
 					(match n with
    					| Empty ->
    						l, false
-   					| Red (a, b, c) ->
+   					| Node (Red, a, b, c) ->
    						let n',m,d = remove_min n in
-   						let t = Red (l, m, n') in
-   						if d then unbalanced_left t else t, false
-						| Black (a, b, c) ->
+   						let t = Node (Red, l, m, n') in
+   						if d then unbalanced_left t 
+							else t, false
+						| Node (Black, a, b, c) ->
 							let n',m,d = remove_min n in
-   						let t = Red (l, m, n') in
-   						if d then unbalanced_left t else t, false		
+   						let t = Node (Red, l, m, n') in
+   						if d then unbalanced_left t 
+							else t, false		
 					)
 			
 	let remove x t =
-    let t', b = remove_aux x t in t'
-	let harness1 () = remove 0 Empty 
+    let t', b = remove_aux x t in t' 
 	
 	(*s The sorted list of elements *)
 
@@ -218,52 +231,16 @@
 		match t with 
     | Empty ->
         accu
-    | Black (l, dv, n) ->
+    | Node (Black, l, dv, n) ->
 				elements_aux (dv :: elements_aux accu n) l
-		| Red (l, dv, n) ->
+		| Node (Red, l, dv, n) ->
         elements_aux (dv :: elements_aux accu n) l
 
   let elements s =
     elements_aux [] s    
-	let harness2 () = elements Empty
-	
-	(*let rec union_aux e1' e2' accu =
-	  match e1', e2' with
-	    | End, End ->
-	        accu
-	    | End, More(x, n, e) ->
-					union_aux End (enum n e) (add x accu)
-	    | More(x, n, e), End ->
-	        union_aux End (enum n e) (add x accu)
-	    | (More(x1, r1, e1)), (More(x2, r2, e2)) ->
-	      if x1 < x2 then union_aux (enum r1 e1) e2' (add x1 accu)
-	      else if x1 > x2 then union_aux e1' (enum r2 e2) (add x2 accu)
-	      else union_aux (enum r1 e1) (enum r2 e2) (add x1 accu)
-	let union s1 s2 =
-		union_aux (enum s1 End) (enum s2 End) Empty
-
-
-	let rec inter_aux e1' e2' accu =
-	  match e1', e2' with
-	    | End, e2' -> accu
-	    | e1', End -> accu
-	    | (More(x1, r1, e1)), (More(x2, r2, e2)) ->
-	        if x1 < x2 then inter_aux (enum r1 e1) e2' accu
-	        else if x1 > x2 then inter_aux e1' (enum r2 e2) accu
-	        else inter_aux (enum r1 e1) (enum r2 e2) (add x1 accu)
-  let inter s1 s2 =
-    inter_aux (enum s1 End) (enum s2 End) Empty
-
-
-	let rec diff_aux e1' e2' accu =
-      match e1', e2' with
-        | End, e2' ->
-            accu
-        | More(x, n, e), End ->
-            diff_aux (enum n e) End (add x accu)
-        | (More(x1, r1, e1)), (More(x2, r2, e2)) ->
-            if x1 < x2 then diff_aux (enum r1 e1) e2' (add x1 accu)
-            else if x1 > x2 then diff_aux e1' (enum r2 e2) accu
-            else diff_aux (enum r1 e1) (enum r2 e2) accu
-  let diff s1 s2 =
-    diff_aux (enum s1 End) (enum s2 End) Empty *)
+		
+	let harness () = 
+		let t = Node (Red, Empty, 0, Empty) in
+		(add 0 t;	
+		elements t;
+		remove 0 t)
